778e1a5e8db18e9d4888b8ecb0b2f25529c10d96
38dh91 <c1sco.m@yandeex.ru>
2024-12-16 16:31:55 +0300

    mv auth headers to shared

--- defaults/grpc.go
+++ defaults/grpc.go
@@ -3,4 +3,10 @@ package defaults
 const (
 	// максимальный размер одного прото сообщения
 	MaxProtobufMessageSize = 2 * 1024 * 1024 * 1024
+	// название заголовка для авторизации листенера
+	GrpcAuthListenerHeader = "X-Access-Token"
+	// название заголовка для авторизации оператора
+	GrpcAuthOperatorHeader = "Y-Access-Token"
+	// название заголовка для авторизации на management сервере
+	GrpcAuthManagementHeader = "Z-Access-Token"
 )
